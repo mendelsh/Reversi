f70a39d87e9c8da06cda3c35deef9eafe1062250
mendelsh <mendelshperber@gmail.com>
2025-12-04 23:33:29 +0200

    add: Dockerfile

--- Othello-Engine/server/reversi-server/server.py
+++ Othello-Engine/server/reversi-server/server.py
@@ -10,7 +10,7 @@ from othello import Board, Bot
 app = FastApp()
 
 @app.get("/best-move/{bot_type}/{side}/{black_bitboard}/{white_bitboard}/{depth}")
-def best_move(bot_type: str, side: bool, black_bitboard: int, white_bitboard: int, depth: int):
+async def best_move(bot_type: str, side: bool, black_bitboard: int, white_bitboard: int, depth: int):
     # Ensure turn (side) is bool — FastApp may give string ("true"/"false")
     side = bool(int(side)) if isinstance(side, str) else side
 
